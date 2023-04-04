1c46e89c22cdeeb5b033c66d2e3408f5e2e407ce
sergiojulio <sergiojulio@gmail.com>
2023-04-03 20:56:20 -0400

    Reformat file

--- src/main/scala/org/sergiojulio/job.scala
+++ src/main/scala/org/sergiojulio/job.scala
@@ -1,4 +1,5 @@
 package org.sergiojulio
+
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{Level, Logger}
 
@@ -7,10 +8,7 @@ object job {
   def main(args: Array[String]): Unit = {
 
     //Logger.getLogger("org").setLevel(Level.ERROR)
-
     print("\n\n>>>>> START OF PROGRAM <<<<<\n\n")
-
-
     // args(0) path sqlite db
     // args(1) path parquets
 
@@ -27,16 +25,14 @@ object job {
     df.printSchema()
     df.show()
     */
-
     // for tables
-
     val df2 = spark.read.format("jdbc").options(
-        Map("url" -> "jdbc:sqlite:/home/sergio/dev/spark/spark-sqlite-to-parquet/tmp/chinook.db",
-            "query" -> "SELECT name FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%'")).load()
+      Map("url" -> "jdbc:sqlite:/home/sergio/dev/spark/spark-sqlite-to-parquet/tmp/chinook.db",
+        "query" -> "SELECT name FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%'")).load()
 
     df2.show()
 
-    val myTableNames = df2.select("name").collect.map(f=>f.getString(0)).toList
+    val myTableNames = df2.select("name").collect.map(f => f.getString(0)).toList
 
     for (t <- myTableNames) {
 
@@ -45,10 +41,9 @@ object job {
       val str1 = "SELECT * FROM "
       val str2 = t.toString
 
-
       val tableData = spark.read.format("jdbc").options(
         Map("url" -> "jdbc:sqlite:/home/sergio/dev/spark/spark-sqlite-to-parquet/tmp/chinook.db",
-            "query" -> str1.concat(str2))).load()
+          "query" -> str1.concat(str2))).load()
 
       tableData.coalesce(1).write.format("parquet").mode("append").save("/home/sergio/dev/spark/spark-sqlite-to-parquet/tmp/" + t + ".parquet")
 
@@ -56,10 +51,8 @@ object job {
     }
 
     // end for tables
-
     print("\n\n>>>>> END OF PROGRAM <<<<<\n\n")
 
     spark.close()
-
   }
 }
